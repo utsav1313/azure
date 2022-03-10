06419c28e48475bc34421dd2694b13dc88bf9b0e
utsa <utsav>
2022-03-10 17:59:59 +0530

    dev file created

--- main.tf
+++ main.tf
@@ -21,11 +21,12 @@ resource "azurerm_app_service_plan" "utsav_app" {
   name                = "utsav_app"
   location            = azurerm_resource_group.utsav_group.location
   resource_group_name = azurerm_resource_group.utsav_group.name
-
+}
   sku {
     tier = "Standard"
     size = "S1"
  }
+ 
 resource "azurerm_storage_account" "utsav_group" {
   name                     = "demo_storage"
   resource_group_name      = azurerm_resource_group.utsav_group.name
