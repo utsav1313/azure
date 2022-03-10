f379a8ef816b75818fe8fcab983773462e985abf
utsa <utsav>
2022-03-10 17:32:02 +0530

    storage

--- main.tf
+++ main.tf
@@ -17,3 +17,13 @@ resource "azurerm_resource_group" "utsav_group" {
   name     = "utsav_group"
   location = "central india"
 }
+resource "azurerm_app_service_plan" "utsav_app" {
+  name                = "utsav_app"
+  location            = azurerm_resource_group.utsav_group.location
+  resource_group_name = azurerm_resource_group.utsav_group.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
